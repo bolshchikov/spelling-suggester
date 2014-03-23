9b74ec7a9b62ca96ebbc00b180ae6347545ccf63
bolshchikov <sergey@bolshchikov.net>
2014-03-24 01:10:20 +0200

    result is calculated

--- suggester.js
+++ suggester.js
@@ -12,8 +12,26 @@ var QUERIES_PATH = './input/misspelled_queries.csv';
 var FREQUENCY_PATH = './input/word_frequency.csv';
 
 (function () {
+
   Q.all([parser.getQueries(QUERIES_PATH), parser.getFrequencies(FREQUENCY_PATH)])
     .spread(function (queries, frequencies) {
-      distance.calculate('vaps', 'lap');
+      var res = {}, freq_keys = Object.keys(frequencies);
+      for (var i = 0, leni = queries.length; i < leni; i += 1) {
+        for (var j = 0, lenj = freq_keys.length; j < lenj; j += 1) {
+          var dist = distance.calculate(queries[i], freq_keys[j]);
+          if (dist === 1 || dist === 2) {
+            if (res[queries[i]] === void 0) {
+              res[queries[i]] = [freq_keys[j]];
+            }
+            else {
+              res[queries[i]].push(freq_keys[j]);
+            }
+          }
+        }
+      }
+      return res;
+    })
+    .done(function (result) {
+      console.log(result);
     });
 })();
